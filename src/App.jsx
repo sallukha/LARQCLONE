a9fa579a55fb0b2db9bd52eaadceac348b224bf8
sallu khan <sallukhan54159@gmail.com>
2025-04-11 11:08:58 +0530

    some change

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './componenets/NavBar';
 import Footer from './componenets/Footer';
-
- 
 const Home = lazy(() => import('./pages/Home'));
 const ShopAll = lazy(() => import('./pages/ShopAll'));
 const Pitchers = lazy(() => import('./pages/Pitchers'));
@@ -13,39 +11,26 @@ const Register = lazy(() => import('./pages/Register'));
 const Technology = lazy(() => import('./pages/Technology'));
 const WaterQuality = lazy(() => import('./pages/WaterQuality'));
 const ShowProduct = lazy(() => import('./componenets/ShowProduct'));
-
 const App = () => {
   return (
     <BrowserRouter>
-      <NavBar />
+    <NavBar />
+    <Suspense fallback={<div className="text-center my-10 text-xl">Loading...</div>}>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/shopall" element={<ShopAll />} />
+        <Route path="/shopall" element={<ShopAll/>} />
         <Route path="/pitchers" element={<Pitchers />} />
         <Route path="/bottles" element={<Bottles />} />
         <Route path="/login" element={<LogIn />} />
-        <Route path="/Tech" element={<Technology />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/tech" element={<Technology />} />
         <Route path="/water" element={<WaterQuality />} />
-      </Routes
-
-      
-      <Suspense fallback={<div className="text-center my-10 text-xl">Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/shopall" element={<ShopAll />} />
-          <Route path="/pitchers" element={<Pitchers />} />
-          <Route path="/bottles" element={<Bottles />} />
-          <Route path="/login" element={<LogIn />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/tech" element={<Technology />} />
-          <Route path="/water" element={<WaterQuality />} />
-          <Route path="/products/:id" element={<ShowProduct />} />
-        </Routes>
-      </Suspense>
-
-      <Footer />
->>>>>>> devsallu
-    </BrowserRouter>
+        <Route path="/products/:id" element={<ShowProduct />} />
+      </Routes>
+    </Suspense>
+    <Footer />
+  </BrowserRouter>
+     
   );
 };
 
