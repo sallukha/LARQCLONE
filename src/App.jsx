1030f238bf3fae10438c3e2da2bf78e3ae665050
sallu khan <sallukhan54159@gmail.com>
2025-05-18 00:15:15 +0530

    fix bugs

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './componenets/Footer';
 const Home = lazy(() => import('./pages/Home'));
 const ShopAll = lazy(() => import('./pages/ShopAll'));
 const Pitchers = lazy(() => import('./pages/Pitchers'));
-const Bottles = lazy(() => import('./pages/Bottles'));
 const LogIn = lazy(() => import('./pages/LogIn'));
 const Register = lazy(() => import('./pages/Register'));
 const Technology = lazy(() => import('./pages/Technology'));
@@ -14,22 +13,21 @@ const ShowProduct = lazy(() => import('./componenets/ShowProduct'));
 const App = () => {
   return (
     <BrowserRouter>
-    <NavBar />
-    <Suspense fallback={<div className="text-center my-10 text-xl">Loading...</div>}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/shopall" element={<ShopAll/>} />
-        <Route path="/pitchers" element={<Pitchers />} />
-        <Route path="/bottles" element={<Bottles />} />
-        <Route path="/login" element={<LogIn />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/tech" element={<Technology />} />
-        <Route path="/water" element={<WaterQuality />} />
-        <Route path="/products/:id" element={<ShowProduct />} />
-      </Routes>
-    </Suspense>
-    <Footer />
-  </BrowserRouter>
+      <NavBar />
+      <Suspense fallback={<div className="text-center my-10 text-xl">Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shopall" element={<ShopAll />} />
+          <Route path="/pitchers" element={<Pitchers />} />
+          <Route path="/login" element={<LogIn />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/tech" element={<Technology />} />
+          <Route path="/water" element={<WaterQuality />} />
+          <Route path="/products/:id" element={<ShowProduct />} />
+        </Routes>
+      </Suspense>
+      <Footer />
+    </BrowserRouter>
   );
 };
 export default App;
